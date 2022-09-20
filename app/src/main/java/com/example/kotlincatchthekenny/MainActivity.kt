73032dd4acbbbdf7f1e97633b32dfab1795f9513
kutayozcnn <80099601+kutayozcnn@users.noreply.github.com>
2022-09-20 11:42:28 +0300

    Spaces have been removed and code has been made readable.

--- app/src/main/java/com/example/kotlincatchthekenny/MainActivity.kt
+++ app/src/main/java/com/example/kotlincatchthekenny/MainActivity.kt
@@ -12,19 +12,15 @@ import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
-
 class MainActivity : AppCompatActivity() {
     var score = 0
     var ImageArray = ArrayList<ImageView >()
     var handler = Handler()
     var runnable = Runnable {}
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         ImageArray.add(imageView1)
         ImageArray.add(imageView2)
         ImageArray.add(imageView3)
@@ -39,69 +35,42 @@ class MainActivity : AppCompatActivity() {
         ImageArray.add(imageView12)
 
         hideImage()
-
         //CountTimeTimer
         object : CountDownTimer(15500,1000){
-            override fun onTick(p0: Long) {
-                textTime.text = "Time ="+ p0/1000
-            }
-
-
-
+            override fun onTick(p0: Long) { textTime.text = "Time ="+ p0/1000 }
             override fun onFinish() {
-
-
                 handler.removeCallbacks(runnable)
-
-                for (image in ImageArray){
-                    image.visibility = View.INVISIBLE
-                }
+                for (image in ImageArray){ image.visibility = View.INVISIBLE }
                 //alert
                 val alert = AlertDialog.Builder(this@MainActivity)
-
                 alert.setTitle("Game Over!")
                 alert.setMessage("Restart The Game ?")
                 alert.setPositiveButton("Yes",DialogInterface.OnClickListener { dialogInterface, i ->
-
                 val intent = intent
                     finish()
                     startActivity(intent)
-
                 })
                 alert.setNegativeButton("No",DialogInterface.OnClickListener { dialogInterface, i ->
-
                     Toast.makeText(this@MainActivity,"GAME OVER!",Toast.LENGTH_LONG).show()
-
                 })
                 alert.show()
             }
         }.start()
-
     }
-
-
     fun hideImage(){
-
         runnable = object :Runnable{
             override fun run() {
-                for(image in ImageArray){
-                    image.visibility = View.INVISIBLE
-                }
+                for(image in ImageArray){ image.visibility = View.INVISIBLE }
                 val random = Random
                 val randomIndex = random.nextInt(13)
                 ImageArray[randomIndex].visibility = View.VISIBLE
-
                 handler.postDelayed(runnable,500)
             }
-            }
-            handler.post(runnable)
         }
-
-
-
+            handler.post(runnable)
+    }
     fun increaseScore(view : View){
         score++;
         textScore.text = "Score = $score"
-
     }
 }
